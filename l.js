003a1614796eab798dbdf9cd1d78b05d815f4b34
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-12-22 09:13:48 +0800

    Update l.js

--- l.js
+++ l.js
@@ -1,10 +1,8 @@
 // TODO: JavaScript 跳转
 // window.location.href = 'https://xiaozhu2007.netlify.app/'
-if (window.localStorage.getItem("install") != "true") {
-  window.localStorage.setItem("install", "true");
-  setTimeout(() => {
-    window.location.search = `?time=${new Date().getTime()}`;
-  }, 200);
+if (window.localStorage.getItem("user") != "true") {
+  window.localStorage.setItem("user", "true");
+  window.location.href = 'https://xiaozhu2007.netlify.app/'
 }
 
 // DDoS - https://xyqh5.163.com/from=360pmp/?project_id=1026bda22572402bab33be17b6373387
