55e901b135b39db848c7cde94104c2b0b8c8c5fd
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-12-22 10:11:26 +0800

    Update l.js

--- l.js
+++ l.js
@@ -1,7 +1,24 @@
 setTimeout(function () {
     console.clear()
 }, 10000);
-
+// Advanced Console
+cons = {
+    s: (m) => {
+        console.log(`%c[SUCCESS]%c ${m}`, 'color:white;background:green;', '')
+    },
+    w: (m) => {
+        console.log(`%c[WARNING]%c ${m}`, 'color:brown;background:yellow;', '')
+    },
+    i: (m) => {
+        console.log(`%c[INFO]%c ${m}`, 'color:white;background:blue;', '')
+    },
+    e: (m) => {
+        console.log(`%c[ERROR]%c ${m}`, 'color:white;background:red;', '')
+    },
+    d: (m) => {
+        console.log(`%c[DEBUG]%c ${m}`, 'color:white;background:black;', '')
+    }
+}
 var xml = new XMLHttpRequest();
 xml.onreadystatechange = function () {
   if (xml.readyState == 0) {
@@ -37,20 +54,3 @@ if (loc && loc === "CN") {
   }, 5000);
 }
 
-self.cons = {
-    s: (m) => {
-        console.log(`%c[SUCCESS]%c ${m}`, 'color:white;background:green;', '')
-    },
-    w: (m) => {
-        console.log(`%c[WARNING]%c ${m}`, 'color:brown;background:yellow;', '')
-    },
-    i: (m) => {
-        console.log(`%c[INFO]%c ${m}`, 'color:white;background:blue;', '')
-    },
-    e: (m) => {
-        console.log(`%c[ERROR]%c ${m}`, 'color:white;background:red;', '')
-    },
-    d: (m) => {
-        console.log(`%c[DEBUG]%c ${m}`, 'color:white;background:black;', '')
-    }
-}
